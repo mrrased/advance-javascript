014e90713d98abdbe7d079d74698a683dd2b0090
mrrased <rashedtechdr.786@gmail.com>
2020-03-19 14:16:20 +0600

    eidit done

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,13 +1,19 @@
-// let pakhi;
+let pakhi;
 function add(num1, num2){
     console.log(num1 + num2);
     return//value isn't defined result is undefined
 }
 
-// function add(num1, num2){
-//     console.log(num1,num2);
-// }
+function add(num1, num2){
+    console.log(num1,num2);
+}
 
 
 const premik = {name:"smart", phone:13456};
 console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages =[2, 5, 11]
+console.log(ages[13]);
