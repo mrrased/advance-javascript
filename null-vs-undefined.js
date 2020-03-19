0f149f8230ba9165c9147c9a2f3cda8843cf3c20
mrrased <rashedtechdr.786@gmail.com>
2020-03-19 14:15:39 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,13 @@
+// let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return//value isn't defined result is undefined
+}
+
+// function add(num1, num2){
+//     console.log(num1,num2);
+// }
+
+
+const premik = {name:"smart", phone:13456};
+console.log(premik.gf);
---
+++ true-false.js
@@ -0,0 +1,21 @@
+//falsy
+//""
+//false
+// 0
+// undefined
+// mull
+// NaN
+// truthy
+// '0'
+// ' '
+// []
+// 'false'
+
+let name = 0;
+// if(name)
+if (name || name ==0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
